93093d17f1cbf93d860e094c5f7201bccaa0d1a3
ethonwilles <ethonwilles@gmail.com>
2020-04-21 13:34:04 -0600

    asdf

--- __init__.py
+++ __init__.py
@@ -579,9 +579,9 @@ def take_screenshot():
     driver = webdriver.Chrome(options=options)
     driver.get('https://{}.com'.format(url))
     sleep(1)
-    driver.find_element_by_tag_name('body').screenshot('./static/screenshots/{}.png'.format(url))
+    driver.find_element_by_tag_name('body').screenshot('./static/{}.png'.format(url))
     driver.quit()
-    return send_from_directory("./static/screenshots", filename="{}.png".format(url))
+    return send_from_directory("static", filename="{}.png".format(url))
     #     else:
         
     #         options = Options()
